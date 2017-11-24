279d331c9657e622460f153734ee49f06cbb61c9
Yusaku Kasahara <yusakukasahara@gmail.com>
2017-11-24 15:23:10 -0500

    added bcrypt user auth

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -6,7 +6,9 @@ var errors = {
   msg1: 'Username taken!',
   msg2: 'Usernames must be at least 5 characters long.',
   msg3: 'Passwords must be at least 8 characters long.',
-  msg4: 'Usernames and passwords may not contain spaces.'
+  msg4: 'Usernames and passwords may not contain spaces.',
+  msg5: 'That password and username did not match. Please try again.',
+  msg6: 'That username does not exist. Sign up below.'
 }
 
 const createSession = (req, res, newUser) => 
@@ -45,10 +47,39 @@ exports.createNewUser = (req, res) => {
   })
 };
 
-exports.loggedUser = (req, res) => {
+const checkUser = (req, res) => {
+  isLoggedIn(req) ? createSession(req, res, req.session.user) : res.sendStatus(404);
+};
 
+exports.loggedUser = (req, res) => {
+  checkUser(req, res);
+};
+
+exports.logout = (req, res) => {
+  req.session.destroy();
+  return res.send(null);
 };
 
 exports.authUser = (req, res) => {
+  User.find({username : req.body.username}, (err, match) => {
+    if (err) {
+      res.status(403).send(err);
+    } else {
+      if (match.length === 1) {
+        var hashed = match[0].password;
+        var input = req.body.password;
 
+        bcrypt.compare(input, hashed, (err, isMatching) => {
+          if (isMatching) {
+            var user = match[0];
+            createSession(req, res, user);
+          } else {
+            res.status(403).send(errors.msg5)
+          }
+        });
+      } else {
+        res.status(400).send(errors.msg6);
+      }
+    }
+  });
 };
